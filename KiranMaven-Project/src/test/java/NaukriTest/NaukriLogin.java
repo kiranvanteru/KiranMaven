1b5f0854dceef6ae93afbfed5b3baa2c11914305
kiranvanteru <kk23vanteru@gmail.com>
2017-04-09 18:24:27 +0530

    Test data provider

--- KiranMaven-Project/src/test/java/NaukriTest/NaukriLogin.java
+++ KiranMaven-Project/src/test/java/NaukriTest/NaukriLogin.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
@@ -14,6 +15,7 @@ import browserScreen.Screenshots;
 import com.Naukri.PageObjects.Login;
 import com.Naukri.PageObjects.NaukriHome;
 
+import utils.Exceldata;
 import utils.InitializeBrowser;
 
 public class NaukriLogin {
@@ -21,32 +23,38 @@ public class NaukriLogin {
 	
 	Login nlog;
 	NaukriHome mnh;
-  @Test
-  public void f() throws Exception {
+
+	
+	
+	 @BeforeTest
+	  public void beforeTest() {
+		  
+		  driver=InitializeBrowser.OpenDriver("firefox");
+		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		  driver.manage().window().maximize();
+		  
+	  }
+	
+ @DataProvider(name="authentication")
+
+ public Object[][] testdata(){
+	 return Exceldata.GetExcelData("D:\\TestData1.xls", 0);
+	
+ }
+  @Test (dataProvider="authentication")
+  public void f(String username, String pwd) throws Exception {
 	  driver.get("https://naukri.com");
 	  System.out.println(driver.getTitle());
 	  
 	  nlog=PageFactory.initElements(driver, Login.class);
 	  mnh=PageFactory.initElements(driver, NaukriHome.class);
-	  nlog.LoginNaukriHome("kiranv23", "kiran1234");
+	  nlog.LoginNaukriHome(username, pwd);
 	  Screenshots.driverScreen(driver, "NaukriHome");
 	  mnh.MyHomePage();
 	  Screenshots.driverScreen(driver, "MyNaukri");
 	  System.out.println("title of the page is "+driver.getTitle());
   }
-  @BeforeTest
-  public void beforeTest() {
-	  
-	  driver=InitializeBrowser.OpenDriver("firefox");
-	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  driver.manage().window().maximize();
-	  
-	 
-//	  driver=new FirefoxDriver();
-	  
-	  
-	  
-  }
+ 
 
   @AfterTest
   public void afterTest() {
